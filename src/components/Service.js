7e414c62232796db1eaf954edd4b81ad4d5b02e6
Stephen Demaine <stephendemaine@gmail.com>
2023-09-05 01:54:24 -0400

    fixed babel plugin bug

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,13 +1,12 @@
-const Service = (props) => {
+const Service = ({name, description, icon}) => {
 
-    const {name, description, icon} = props
     return (
         <article className="service">
             <span className="service-icon"><i className={icon}></i></span>
             <div className="service-info">
-                <h4 className="service-title">{props.name}</h4>
+                <h4 className="service-title">{name}</h4>
                 <p className="service-text">
-                    {props.description}
+                    {description}
                 </p>
             </div>
         </article>
